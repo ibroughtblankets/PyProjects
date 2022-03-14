c1a167390887d3606553286c104f143921b58deb
dspucch <dom.spucches@gmail.com>
2022-03-13 22:38:34 -0400

    Shorter Code

--- Love.py
+++ Love.py
@@ -1,12 +1,10 @@
 import time
 import sys
 
-name = input("What is your name?")
+love = "I Love You \n"
+name = input("What is your name? ")
 
 if name == name:
-    print("I"), time.sleep(2.0)
-    print("Love"), time.sleep(2.0)
-    print("You"), time.sleep(2.0)
 
     def delay_name(n):
         for e in n:
@@ -14,4 +12,5 @@ if name == name:
             sys.stdout.flush()
             time.sleep(0.25)
 
+    delay_name(love)
     delay_name(name)
